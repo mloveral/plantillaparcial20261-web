2fcad30f10e62696fbad0c795dc46607c3f4a00e
Manu_Love <107737902+mloveral@users.noreply.github.com>
2026-05-12 07:54:36 -0500

    guard faltante

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller,Param,ParseIntPipe,Post } from '@nestjs/common';
+import { Body, Controller,Param,ParseIntPipe,Post, UseGuards } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { ApiKeyGuard } from 'src/auth/api-key.guard';
 
 @Controller('posts')
+@UseGuards(ApiKeyGuard)
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
